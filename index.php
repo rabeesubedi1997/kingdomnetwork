666fb5594b82ad00f3a1bd586d09d8adad03726d
rabindrasubedi1997 <rabindra@ims.edu.np>
2026-07-20 12:55:42 +1000

    Fix cPanel deployment: proxy API to artisan serve, fix index.php paths

--- index.php
+++ index.php
@@ -5,48 +5,13 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../kingdomnetwork/backend/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../../kingdomnetwork.kitetool.com/backend/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| changes in the future.
-|
-*/
+require __DIR__.'/../../kingdomnetwork.kitetool.com/backend/vendor/autoload.php';
 
-require __DIR__.'/../kingdomnetwork/backend/vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$app = require_once __DIR__.'/../kingdomnetwork/backend/bootstrap/app.php';
+$app = require_once __DIR__.'/../../kingdomnetwork.kitetool.com/backend/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
